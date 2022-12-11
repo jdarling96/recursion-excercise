a303de3a55a918f1b143bcc336214407087bdceb
Josh Darling <joshua.a.darling@gmail.com>
2022-12-11 12:23:52 -0700

    gather strings problem complete all tests passed

--- recursion.js
+++ recursion.js
@@ -22,40 +22,68 @@ function longest(words, i = 0) {
 
 /** everyOther: return a string with every other letter. */
 
-function everyOther(str, i=0) {
-  if(i >= str.length) return ""
+function everyOther(str, i = 0) {
+  if (i >= str.length) return "";
 
-  return str[i] + everyOther(str, i + 2)
+  return str[i] + everyOther(str, i + 2);
 }
 
 /** isPalindrome: checks whether a string is a palindrome or not. */
 
-function isPalindrome(str,starting="",i=0,startingIdx=0) {
-  if(i === isPalindrome.length) return starting[startingIdx]
-
-  let check = isPalindrome(str, starting += str[i], i + 1)
- if(startingIdx + 1 === i){
-  return true
- }
-  if(str[i] === check){
-    return starting[startingIdx + 1]
-  }else{
-    return false
+function isPalindrome(str, i = 0, reversed = []) {
+  reversed.push(str[i]);
+  if (i === str.length - 1) {
+    if (str[i] === reversed[0]) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
+  return isPalindrome(str, i + 1, reversed);
+  
 }
 
 /** findIndex: return the index of val in arr (or -1 if val is not present). */
 
-function findIndex(arr, val) {}
+function findIndex(arr,target,i=0) {
+  if(arr[i] === target) return i
+  if(i === arr.length) return -1
+
+  return findIndex(arr, target, i + 1)
+}
 
 /** revString: return a copy of a string, but in reverse. */
 
-function revString(str) {}
+function revString(str, i=0) {
+  if(i === str.length) return ""
+
+  return revString(str, i + 1) + str[i]
+}
 
 /** gatherStrings: given an object, return an array of all of the string values. */
 
-function gatherStrings(obj) {}
+function gatherStrings(obj) {
+  let returnVals = []
+
+  function _gatherStrings(obj){
+    for(let prop in obj){
+      if(typeof obj[prop] === 'string'){
+        returnVals.push(obj[prop])
+  
+      }
+      if(typeof obj[prop] === 'object'){
+        _gatherStrings(obj[prop])
+
+      }
+    }
+    return
+
+  }
+  _gatherStrings(obj)
+  return returnVals
+  
+}
 
 /** binarySearch: given a sorted array of numbers, and a value,
  * return the index of that value (or -1 if val is not present). */
